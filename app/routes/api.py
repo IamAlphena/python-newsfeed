ddb8141ee4f880a9ab19decca611c3c50f4261c6
Alphena <alphena@gmail.com>
2022-03-16 13:30:03 -0700

    updated api routes for error handling

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,8 @@
+from email import message
 from flask import Blueprint, request, jsonify
 from app.models import User 
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,16 +11,24 @@ def signup():
     data = request.get_json()
     db = get_db()
 
-    # create new user
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    try:
+    # attempt to create new user
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
     #save in database
-    db.add(newUser)
-    db.commit()
+        db.add(newUser)
+        db.commit()
+
+    except:
+        print(sys.exc_info()[0])
+
+        #insert failed send error to front end
+        db.rollback()
+        return jsonify(message = 'Signup Failed'), 500
 
     return jsonify(id = newUser.id)
 
